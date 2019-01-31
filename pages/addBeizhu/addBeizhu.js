051f1cfecc75ddbb59ebf47137f24d086ddab277
ichhhhh <871851924@qq.com>
2019-01-31 18:42:54 +0800

    2019/12/31

---
+++ pages/addBeizhu/addBeizhu.js
@@ -0,0 +1,82 @@
+// pages/addBeizhu/addBeizhu.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    // 最大字符数
+    maxTextLen: 50,
+    // 默认长度
+    textLen: 0
+  },
+  getWords(e) {
+    let page = this;
+    // 设置最大字符串长度(为-1时,则不限制)
+    let maxTextLen = page.data.maxTextLen;
+    // 文本长度
+    let textLen = e.detail.value.length;
+
+    page.setData({
+      maxTextLen: maxTextLen,
+      textLen: textLen
+
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
