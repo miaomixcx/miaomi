adb10f47b4ea9400076d608a8f6f6bc482ab7bce
尹巍 <yinweiwork@163.com>
2019-02-16 17:05:20 +0800

    adf

--- app.js
+++ app.js
@@ -1,35 +1,4 @@
 //app.js
 App({
-  onLaunch:function(){
-    wx.getSetting({
-      success(res) {
-        if (!res.authSetting['scope.userLocation']) {
-          wx.authorize({
-            scope: 'scope.userLocation',
-            success() {
-              wx.getLocation({
-                type: 'wgs84',
-                success(res) {
-                  wx.setStorage({
-                    key: 'location',
-                    data: JSON.stringify(res),
-                  })
-                }
-              })
-            }
-          })
-        }else{
-          wx.getLocation({
-            type: 'wgs84',
-            success(res) {
-              wx.setStorage({
-                key: 'location',
-                data: JSON.stringify(res),
-              })
-            }
-          })
-        }
-      }
-    })
-  }
+  
 })
