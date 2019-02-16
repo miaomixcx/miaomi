314af6ebe9b1a0806ee56ab0f398c9022a334bad
尹巍 <yinweiwork@163.com>
2019-02-16 11:58:00 +0800

    fff

--- app.js
+++ app.js
@@ -1,4 +1,35 @@
 //app.js
 App({
-  
+  onLaunch:function(){
+    wx.getSetting({
+      success(res) {
+        if (!res.authSetting['scope.userLocation']) {
+          wx.authorize({
+            scope: 'scope.userLocation',
+            success() {
+              wx.getLocation({
+                type: 'wgs84',
+                success(res) {
+                  wx.setStorage({
+                    key: 'location',
+                    data: JSON.stringify(res),
+                  })
+                }
+              })
+            }
+          })
+        }else{
+          wx.getLocation({
+            type: 'wgs84',
+            success(res) {
+              wx.setStorage({
+                key: 'location',
+                data: JSON.stringify(res),
+              })
+            }
+          })
+        }
+      }
+    })
+  }
 })
